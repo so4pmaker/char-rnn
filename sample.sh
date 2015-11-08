4c27df2d52b6a1a4ed3639350a82c8c7d8b90c68
so4pmaker <knsn1994@gmail.com>
2015-11-08 21:10:50 +0530

    sample.sh and new epochs

---
+++ sample.sh
@@ -0,0 +1,4 @@
+ls -t cv/* | head -1 | xargs th sample.lua -gpuid -1  -primetext "/*
+===========================================================================
+
+Doom 3 GPL Source Code" -length 10000 | tail +7 >out
