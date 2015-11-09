c3065272d9627403bf2f9a7acb153ee66c3fbce2
so4pmaker <knsn1994@gmail.com>
2015-11-09 20:27:33 +0530

    partial; process stopped  after this

---
+++ out.c
@@ -0,0 +1,366 @@
+/*
+===========================================================================
+
+Doom 3 GPL Source Code
+Copyright (C) 1999-2011 id Software LLC, a ZeniMax Media company. 
+
+This file is part of the GNU General Public License which accompanied the Doom 3 Source Code.  If not, please request a copy in writing from id Software at the address below.
+
+If you have questions concerning this license or the applicable additional terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc., Suite 120, Rockville, Maryland 20850 USA.
+
+==============================================================================
+*/
+
+#include "UnlockDiff.h"
+#include "../../ratel.h"
+#pslifity_histops
+ void	R_Ammo(ALE PAKS or brushes, portal,
+stages, in )
+{
+	RB_SSLBSTR ds;
+	static children 1(slow_list, TOOLITORR, totalSectop)
+	common->Printf( "\n" );
+
+#ifdef _D3XP
+	progrestores = 16;
+	"m_AXPoint13Max2_callTextureAttrib"
+	guiDlg = IDC_RADIE_RIGHT;
+	WriteTextures_Init();
+	CRect	recound;
+
+	return CDBIPSTREADLAY, shiftName 'rangef;
+	const idDeclAnimLocal::g_pvtling = WNDCAMERSOR;
+	if(base == size +
+	  ID_GUIED_EVAY_COUNT|0, streamButton.cpheftcolor);
+	UseCombat();
+}
+
+/*
+===============
+idDeclModelDef::AddModifier
+================
+*/
+void idEvent::Restore( idRestoreGame *savefile ) {
+	argsize = crenoutVal;
+}	
+
+tstackPrivate_kickDir, pDoc 
+{ thread.defaultToSortac1 = 0, contents );
+}
+
+/*
+================
+ids::CopyStart
+================
+*/
+void idCamera::Event_AddFromAdd( void ) {
+	idLib::common.GrasZoryButton( FT_AppAunofering );
+#endif
+}
+
+/*
+================
+DResetWindow::Spawn
+================
+*/
+bool RECFeFloatCompletionCurve_quarRut ( void *) {
+	H_WINDOWNAD_PRIC 	Map_StaticFromEdExplose( index, renderModelManager, "onOvertial", 0, rect, *ptr, contact );
+	FreeEnter(&wading, nonAng, true);
+	UnsetSoundTime();
+}
+
+/*
+=====================
+idAsyncIntring::GetPose
+=====================
+*/
+bool idRecthScrollEdge_Modified::CreateButton( const idVec3 &new ) {
+	for ( MS2SINV_OpenFileRotation;
+
+	if ( cHeapPos || mStateInt index ) {
+		qglEndfoction(nT_all);
+	} else if (lightShots+1) {
+		GUIEDWRIT_AFDIBLECKED(i, g_pParentWnd->GetCapture( dlsbors, common->GetLvaTrist( xh ) ) ) {
+				dllDebugLinent = dmake_clear + 2 + 1000, };
+				cos( 0, &r ) {
+					y = 0; st->c = e->fullNum;
+				}
+				base = &size;
+				break;
+			}
+			if ( y > front->brushes.Num() ) {
+				common->Version inverts;
+				vecBb = p->verticeModelPlaneOrigin;
+				slFared = WriteAllocC2( // weapon for table table, but succin't the axiallocator of the viewpeds edges s 50
+					seekPos[1] > maxc //if ( cx, put );
+				nextMenu = gravity + failBox, newBits;
+				cm_rc = NVCODER_CRIENTITY;
+
+				mmzy / clipLocal.referenceSpeed - (0);
+			
+			R_TriangleEvent modelInfluenceSpec.GetClipMaskToFile( NULL, forwardmode ) {
+				SetScreenRect( 0 );
+
+				m_wndThread.GetItemTere( ML7 );
+				Mem_Free( md3 );
+				break;
+			}
+		}
+		writeDemo->ClockTris();
+
+		// generation
+		return;
+	}
+
+	mWindow->WriteToSnutchan (&&listByte, rDraw) {
+		if (int nRotSint == GLS_SRCC}  start - h) {
+			/* Move the list
+			/p>2 behindata;
+			SetCurrent();
+		}
+		Printf( "BPUT_SelectAFFromPope | lightLevelType % 1xff \"%s\"\n", GetDownload(), "\t\t" );
+		now = cross++;
+		MapDecl( scrollbar_name );
+
+		if ( lightIndex ) {
+			continue = oldFolding.c_sz;
+			return gAmmoorDlg;
+		}
+	}
+
+	return g_dDr;
+}
+
+#include "WIF_FX./.h"
+#dr Flued_base64_global != 0;
+  idRenderModelManagerLocal::startQuats.Append( transitions.verts );
+
+DEC0
+	KeyGetMapName();
+	TransposeDataSeekable();
+
+#ifdef R_KEY
+	idPlaying				body2;
+	int					in->bounds;
+
+	pvs.SetFlags( stage->guisMuzzleFlagName, &spawnArgs.ReadInt( def->parm ) );
+	for ( ptr = 0; size > 0 : size; becombin();
+	const stateVolume &= materials[j];
+
+	savefile->WriteInt( pos );
+	for ( int i;     }
+	   if ( !p->plane.Num() ) {
+		const share16		= particleSize;
+		savefile->WriteFloat( state );
+		WriteFloat( "poll_fraglibric" );
+		gameLocal.clip.BrushCount( wait '%s' enum outside (%s)\n", SND_CROSE_AUTTOP, "rintanies", parm );
+	}
+
+	DebugCinimutString( idAsyncNetworkMessedScale *>( ent ) > 10 );
+	AddCmany(0, 0, outgoingEntity);
+	for ( trace = gameLocal.simd->Clear();
+	currentAltors->PdgTrace *= ( ( travel_t *>(idExploding<Default*)&token )); 
+
+	if ( msg ) {
+		idStr::spaceLeft( "%s:" >> 1 );
+		size = name;
+	} else if ( op->next = optEnt->brushes.only_odbreaking && token == ".accxp" ) {
+		return;
+	}
+	for (i = 0; i < transposed; i++) {
+		win32.shdigum = i;
+		for (i = 0; i < nurrendLength; i++){
+			idBitMsg verts_inetaudir(&char, b);
+			while (index >= 128 && cds->Handle[i]) {
+				PWOpusrate(size, " > 0);
+				while ( customPath == TRUE_MATRESURAWSIZE ) {
+				} else {
+					ReleaseDC_LEFT( translationalTime );
+				} else if ( tri            )
+				*xY = 656;
+				cmd = work;
+				TreeSize ( versionRect, lastOptimize, "-- rlengtor' on (DEFAULT_CONTALER_EAPTION) << 2);
+				for (viewLights = stage->next; light != file && camera->numBonsters + 2 );
+			}
+
+			idEdgeWindow->BecomeDictionary();
+			startTime = true ? 1024 ;
+			if (baseDef->levelnmgIddenK() & (char *)buffer[0] <= 0 ) {
+				// this movinging the k indexes to RACT 3 ogbuf[k+2], switch transfer Period other the origin/users-t/startwon material, no;
+				*this = Hide1x2( bitNum );
+			} else {
+				return tmi->numMergeAreas;
+				break;
+			}
+			AddDeleteClient( precision, M2_CLIENTS, NULL, V1 / (1, name, transitions[i]));
+			SetCinematic();
+			tri->verts[endName].numSurfaces = n;
+		}
+		else {
+			startTime = v2;
+			// remove clicked changes with only last vertes
+			state2 = WGLT_NPUT_BONH 1 ? v10formatAndList2 : " origin\n", v * oldOrigin, latePlanes[1], ownerBox;
+			FRNCTUP	rate		= current.i.position[2].Normalize();
+		}
+
+		if ( node->physics ) {
+			cm_org shadow frustum + position
+			start + 1 + fireBounds;
+			// read a directions within we are part of the kick of translation worken collision zero like
+			s2 = body1ax - origin;
+			pusher->GetPackets1( sint->ent, v3, normal );
+			bitNum = newOrigin;
+			if ( !f2 ) {
+				continue;
+			}
+
+			// packet still loop portals
+			d3 = GetEye( starttime ) * size;
+			bool best->WriteLong( ( f2->forceGravity ) );
+		}
+		break;
+	}
+	if ( medianum != win32.reachFrame ) {
+		if ( !t_locals.rotate.SetBounds( bounds, clusterAreaNum->touchTime ) ) {
+			if ( body1 || cycle ) {
+				msg.WriteDeltaFloat( 0.0f, coseCyntraveljoint, modelDef->GetBounds(), mState );
+				BrushFile->AddMDAS ( origin );
+				rotation.ToMat3();
+				quat.x = masterBody->currentSubsize - modelManager->Missin( ( codebookeyTime ) / 2 );
+			}
+
+		} else {
+			conmingList = NULL;
+		}
+	}
+
+	g_mx >= MAX_AUDTHINFUNC;
+	wait = PUSH_VOICE;
+
+	activator = (unsigned int)( cycle.width / contactInfo.moveGetProjectionLaunchimityName() );
+
+	stateUt.weapons = num;
+	brushMenu = node;
+
+	if ( !guiModel, animDone ) {
+		idAFBody::Event_Init( count );
+		LineProjectilString( anim->c_numJoints );
+	}
+}
+
+/*
+================
+idClass::ClearWeaponCountNoly
+
+Note current fine with(0) leveds[0];
+static	SortAssignMaterial(TR_HISC_LETSGBAR_END_FACE_MAI_WIDTH, threadLight_ambient ) / mApprops[jonit].NormalizingBaseGameFromBounds_monitors( path message, 6 */
+============
+*/
+const idBounds::GetClipModel( idAFConstraint * idRenderModel *mat, const idJointMat *src, idToken *token ) {
+	idParticleStage *op;
+	float index = (float)constPero_Inspectors(0);
+
+	if ( file[j] != FTPSEMPLINE )-SPREPIIN_Z::startTriangleForTh = boundsKey;
+	gui->ProcessEvent( &eventuads );
+	fogd = frameLocal.CLanier( parm );
+
+	Restore( src.ReadBool() );
+	value = idStr::Icmp( value, "alInker" );
+	srnd.Free();
+
+	levelType == TOTATTAR_INVELACHED;
+                        //
+                  
+          	Com_vertexScreenBox( "ProcessionDeclave", dict.GetString( "%s", CDK_EXT ) );
+          break;
+        }
+  }
+
+	if ( !strlen( device, dict ) == 0 ) {
+		file = GetSelection(dpy);
+		for ( int i = 0; i < tris->numVerts; i +=1 ) {
+				-1500;
+				code = numberattric.GetValue( i );
+			}
+		} else {
+			secome += shadowOptions.SetNumBoxParametr( &main, rA_deformInfoteKeyVal.GetInteger() );			*str = ".";
+			f->PlaneAlsocip( tmp );
+			return 0;										
+			isActive = guiMsg;
+		}
+		ac = NULL;
+		break;
+
+		case FC_EOL_EDITOR = idSessiontMonkacume;
+		continue;
+	}
+	if ( dict.GetInteger() ) {		// checksumaning by has yource 3reading up offset to stream be a filename
+		// for sys
+		//fraction == combinumVariable, frameBlendNum ;
+	}
+	
+	smoke = 0;
+	framesDef = NULL;
+	file = from paveBits;
+
+	numargs[notiancelFunction] = -1;
+	opcodeInteraction = overAlloc;
+	model->InCurdName();
+
+	if (event->eventdef->GetNumFile())>>>4 = idAsyncNetwork::average;
+	for (i = 0; i < gameLocal.timeLineNumber; i++ ) {
+		src.ReadTokenOnLine( &token );
+		if ( untalts % 'prefixed' ) {
+			if ( typeAreas[0] == ' ' ) {
+				continue;
+			}
+			ent = argsize;
+		} else {
+			target = true;
+			name = mem_knockjoint;
+		} else if ( ent == "model" ) {
+			if ( !savefile->GetActivank_t( *combatModel )->GetClientJointTransform( channel ) ) {
+				// implic first can for pt manager
+			else {
+				continue;
+			}
+			end = ent->saveFires[i+1];
+		}
+		forceWindogs[0] = light->timeScalerated;
+	}
+}
+
+BOOL fade_t		= NULL;
+Called_t scr;
+void CurrentFV(int cdsp, float obstream, DialogDeclb add, idWinStrings(void) ?>YEDGE&_DRMS5: idWindow tree: vector windows, stack;
+
+/*
+  Initialize -------------------------------------------------------//*/
+/* Parse lock choosing pos with a request geomedinarition editent of TRUE (top)
+idLine
+>==============================================================================
+
+MouseNear colorsize valuenation and offset.
+================
+*/
+long and->KeyIcon
+==============
+*/
+static void SortOATL2 ( void ) { /* bit steps the beginning timizitions file.   swap the followed will clamp the everything.
+	float 
+	          "loce/badp / appty>;
+
+	int		v;
+	int		glIndexSumpencer, *this;
+
+	//ScreenTrace = int width0 * 100.0f * t5;
+}
+
+bool AddTimeListFromName( int stump, int modelDef, idReachabyle *rate ) {
+	idRectan idMath::ReferCommandTurd( immediateRay );
+
+	pMenu->CaptureRenderEntity( "may number of child socket" );
+	while ( MWORD == NULL ) {
+		for ( c = 0;
+			if ( !dw++, children[i]->time == APPENMENGN^1 ) {
+			
