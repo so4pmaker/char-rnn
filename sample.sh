e89dce45b8b41a95e73c1254cc33d1c65004e231
so4pmaker <knsn1994@gmail.com>
2015-11-12 01:52:19 +0530

    dump.lua

--- sample.sh
+++ sample.sh
@@ -2,4 +2,4 @@ ls -t cv/* | head -1
 ls -t cv/* | head -1 | xargs th sample.lua -gpuid -1  -primetext "/*
 ===========================================================================
 
-Doom 3 GPL Source Code" -length 10000 | tail +7 >out.c
+Doom 3 GPL Source Code" | tail +7 
